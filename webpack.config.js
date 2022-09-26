786a5795f119392b8ce57e0169f6f6ee7fdc8db7
heyset <c.inaciomatheus@gmail.com>
2022-09-26 15:28:53 -0300

    ci: improves build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
 const fs = require('fs');
 const path = require('path');
+const semver = require('semver');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    main: { import: './src/extensions/chrome/main.ts', filename: 'filterdin.js' },
-    popup: { import: './src/extensions/chrome/popup.ts', filename: 'popup.js' },
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist', 'chrome'),
-  },
-  resolve: {
-    extensions: ['.ts'],
-  },
-  module: {
-    rules: [
+module.exports = (env, arg) => {
+  if (!semver.gte(process.versions.node, '16.7.0')) {
+    throw new Error('Node version must be 16.7.0 or higher');
+  }
+  
+  const browser = env.browser;
+  if (!browser) {
+    throw new Error('Must specify browser: either chrome or firefox');
+  }
+
+  return {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+      main: { import: `./src/extensions/${browser}/main.ts`, filename: 'filterdin.js' },
+      popup: { import: `./src/extensions/${browser}/popup.ts`, filename: 'popup.js' },
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist', browser),
+    },
+    resolve: {
+      extensions: ['.ts'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
+    plugins: [
       {
-        test: /\.ts/,
-        loader: 'ts-loader',
-      },
-    ],
-  },
-  plugins: [
-    {
-      apply: (compiler) => {
-        compiler.hooks.afterCompile.tap('MyCutePlugin_compile', (params) => {
-          const outputPath = params.compiler.outputPath;
-          fs.cpSync(path.resolve(__dirname, 'src', 'static'), outputPath, { force: true, recursive: true });
-          fs.cpSync(path.resolve(__dirname, 'src', 'extensions', 'chrome', 'static'), outputPath, { force: true, recursive: true });
-        });
-      },
-    }
-  ]
-};
+        apply: (compiler) => {
+          compiler.hooks.afterCompile.tap('MyCutePlugin_compile', (params) => {
+            const outputPath = params.compiler.outputPath;
+            fs.cpSync(path.resolve(__dirname, 'src', 'static'), outputPath, { force: true, recursive: true });
+            fs.cpSync(path.resolve(__dirname, 'src', 'extensions', browser, 'static'), outputPath, { force: true, recursive: true });
+          });
+        },
+      }
+    ]
+  };
+}
